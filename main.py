ca7a0307fab90fed68fb98f2b2c614520aa47174
ConquerorKapson <69422202+ConquerorKapson@users.noreply.github.com>
2022-06-22 01:03:36 +0530

    Change in test class

--- main.py
+++ main.py
@@ -1,7 +1,2 @@
-def print_hi(name):
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
+rollNumber = input("Enter the roll number : ")
+print(f"The Roll Number is {rollNumber}")
